ccd74937ca6ba6715d3c23c7b8d4c64dbdcd476f
Brian <yakebrian@gmail.com>
2022-09-12 14:43:01 -0700

    Size matters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,14 +11,14 @@ int main(void)
 {
 	char c;
 	int i;
-	long li;
-	long long lli;
+	long int li;
+	long long int lli;
 	float f;
 
 	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long: %ld byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long: %ld byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
-	return(0);
+	return (0);
 }
