0a394c2b059b4d70f79cdce04399006fc464b0a5
Brian <yakebrian@gmail.com>
2022-09-12 09:08:00 -0700

    Using print for art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h.>
+/**
+ * main - print the string in the function printf
+ *
+ * Description: Using the main function
+ * This program prints with proper grammar, but the outcome is a piece of art,"#include <stdio.h.>
+ */**
+main - print the string in the function printf
+
+Description: Using the main function
+This program prints with proper grammar, but the outcome is a piece of art,"#include <stdio.h.>
+/**
+ * main - print the string in the function printf
+ *
+ * Description: Using the main function
+ * This program prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%.\n",'with proper grammar, but the outcome is a piece of art,');
+	return(0);
+}
