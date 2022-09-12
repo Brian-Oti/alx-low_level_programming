b9c5f533c11830526ffc9886d2a43e8ae88e678d
Brian <yakebrian@gmail.com>
2022-09-12 09:54:12 -0700

    What is the size?

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of char:%ld byte(s)\n, sizeof(c));
+	printf("Size of int:%ld byte(s)\n, sizeof(i));;
+	printf("Size of long:%ld byte(s)\n, sizeof(li));
+	printf("Size of long long:%ld byte(s), sizeof(lli));
+	printf("Size of float:%ld byte(s)\n, sizeof(f));
+	return(0);
+}
